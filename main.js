ee10d1edd2f97e8a3d2d1dc08a474bd9e90410ac
pablo leal <leal.jpablo94@gmail.com>
2023-07-04 23:26:25 +0200

    1 card's generation

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("hello");
+const generateCheap = (values, suits) => {
+  const cheap = [];
+
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      const card = values[j] + suits[i];
+      cheap.push(card);
+    }
+  }
+  return cheap;
+};
+
+const values = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A",
+];
+const suits = ["♠️", "♣️", "♦️", "♥️"];
+
+const cheap = generateCheap(values, suits);
+console.log(cheap);
